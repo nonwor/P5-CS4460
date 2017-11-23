eb4d36755ab3520bcbe5adb6ff581a403c3624d8
non <non@Sataporms-MacBook-Pro.local>
2017-11-23 17:31:06 -0500

    see through circle

--- js/main.js
+++ js/main.js
@@ -9,16 +9,16 @@ var xScale = d3.scaleLinear()
 
 //Y scale is the cost to attend 
 var yScale = d3.scaleLinear()
-	.range([0,600]);
+	.range([0,500]);
 
 // get radius of the size of the school
 var rScale = d3.scaleSqrt()
 	.range([1,15]);
 
 // color by by region. 9 regions 
-var colorScale = d3.scaleQuantize()
-	.range(['aquamarine','beige','lightsalmon','lightseagreen'
-		,'lightskyblue','limegreen','burlywood','crimson','darkmagenta']);
+// var colorScale = d3.scaleQuantize()
+// 	.range(['aquamarine','beige','lightsalmon','lightseagreen'
+// 		,'lightskyblue','limegreen','burlywood','crimson','darkmagenta']);
 
 //load data into dataset 
 d3.csv('./data/colleges.csv', function(error, dataset){
@@ -26,7 +26,7 @@ d3.csv('./data/colleges.csv', function(error, dataset){
 	colleges = dataset;
 	//console.log(colleges);
 
-	// find the max compute the domain
+	// find the max compute the domain for variables **********************************
 	var radiusMax = d3.max(colleges, function(d){
 		return +d['Undergrad Population'];
 	});
@@ -43,15 +43,39 @@ d3.csv('./data/colleges.csv', function(error, dataset){
 		return +d['Admission Rate'];
 	});
 	console.log(adminRate[1],adminRate[0]);
-	xScale.domain(adminRate[1],adminRate[0]);
+	xScale.domain(adminRate);
 
-	var region = d3.nest()
-		.key(function(d){
-			return d.Region;
+	///*******************************************************************
+
+	svg.selectAll('.Name')
+		.data(colleges)
+		.enter()
+		.append('circle')
+		.attr('class', 'college')
+		.attr('r', function(d){
+			return rScale(d['Undergrad Population']);
 		})
-		.entries(dataset);
+		.attr('cx', function(d){
+			return xScale(d['Admission Rate']);
+		})
+		.attr('cy', function(d){
+			return yScale(d['Average Cost'])
+		})
+		.attr('transform', 'translate(50,50)')
+		.style('fill', function(d){
+			console.log(d.Region);
+			if (d.Region == "Far West") return 'aquamarine' ;
+			if (d.Region == "Great Lakes") return 'darkmagenta';
+			if (d.Region == "Great Plains") return 'lightsalmon';
+			if (d.Region == "New England") return 'mediumturquoise';
+			if (d.Region == "Mid-Atlantic") return 'lightseagreen';
+			if (d.Region == "Outlying Areas") return 'lightskyblue';
+			if (d.Region == "Rocky Mountains") return 'limegreen';
+			if (d.Region == "Southeast") return 'burlywood';
+			if (d.Region == "Southwest") return 'crimson';
+		})
+	        .attr('fill-opacity', 0.7)
 
-	console.log(region);
-	colorScale.domain(region);
 
-})
+
+});
